a1c0d500871db930549078645bf03d8ba49bf154
Michelle Catherine Kelly <michellekelly@ku.edu>
2018-07-17 11:29:49 -0500

    modified to pull correct data, streamMetabolizer calculation input estimation done

--- UnitTestingHW/dataLoad.R
+++ UnitTestingHW/dataLoad.R
@@ -1,8 +1,10 @@
 # Checks if datafile for Kansas River at Desoto Kansas exists locally. If datafile of stream observations not present, pulls data from USGS and merge with NEON light data file. If datafile is present, loads local datafile.
 # 
 # Arguments
-# start.date        start date of data pull from APIs. default value is NULL
-# end.date          end date of data pull from APIs. default value is NULL
+# sitecode         character string, USGS unique identifier for stream gauge stations
+# startdate        start date of data pull from APIs. default value is NULL
+# enddate          end date of data pull from APIs. default value is NULL
+# nickname         characxter string that will be used to name the data file
 # 
 # Output
 #                   dataframe of stream data (discharge, water temperature, 
@@ -16,40 +18,39 @@
 #       add check that if local file does not exist and start/end.date is not 
 #         specified, return error message asking for date specification
 #         can also add this check to Tests.Rmd
-#       if start date end date interval not within par data, return error
-#         add this as a check to tests.Rmd
 
-dataLoad <- function(start.date = NULL, end.date = NULL){
+dataLoad <- function(sitecode, nickname, startdate = NULL, enddate = NULL){
+  # create /data_files/ directory
   #
   # add safety checks here:
   #
   # if local file does not exist, but start/end date is not specified, return error
-  # if start and end date are not contained within available PAR data, return error
   #
-  if(file.exists("./data_files/MetabolismData_KansasRDesotoKS.csv")){
+  if(file.exists(paste0("./data_files/MetabolismData_", nickname, ".csv"))){
     # if local file exists, load local file
-    data <- read.csv("./data_files/MetabolismData_KansasRDesotoKS.csv", header = TRUE)
+    data <- read.csv(paste0("./data_files/MetabolismData_", nickname, ".csv"), header = TRUE)
     # check that dateTime is in proper format
     data$dateTime <- ymd_hms(data$dateTime, tz = "America/Chicago")
     # return dataframe and exit function
     return(data)
   }
-  if(!file.exists("./data_files/MetabolismData_KansasRDesotoKS.csv")){
+  if(!file.exists(paste0("./data_files/MetabolismData_", nickname, ".csv"))){
     # if local file does not exist, pull and compile local file from APIs
-    #
-    site.code <- "06892350" # site code for Kansas River at Desoto, KS
     #
     # parameter codes: see https://nwis.waterdata.usgs.gov/usa/nwis/pmcodes
     #   00010 = water temperature, degrees C
     #   00060 = discharge, ft^3/s
     #   00300 = dissolved oxygen, mg/L
     #   00301 = dissolved oxygen, percent saturation
-    #   = 
     parameter.codes <- c("00010", "00060", "00300", "00301")
     #
     # call to API to pull data
-    data <- readNWISuv(siteNumbers = site.code, parameterCd = parameter.codes,
-                       startDate = start.date, endDate = end.date)
+    data <- readNWISuv(siteNumbers = sitecode, parameterCd = parameter.codes,
+                       startDate = startdate, endDate = enddate)
+    #
+    # add columns of site latitude and longitude (decimal)
+    data$Lat <- attributes(data)$siteInfo$dec_lat_va
+    data$Long <- attributes(data)$siteInfo$dec_lon_va
     #
     # rename columns from codes to human-readable names
     names(data)[names(data)=="X_00010_00000"] <- "WaterTemp_C"
@@ -57,8 +58,9 @@ dataLoad <- function(start.date = NULL, end.date = NULL){
     names(data)[names(data)=="X_00300_00000"] <- "DO_mgL"
     names(data)[names(data)=="X_00301_00000"] <- "DOsat_pct"
     # drop unnecessary columns
-    data <- data[, c("dateTime", "WaterTemp_C", "Discharge_m3s", "DO_mgL", "DOsat_pct")]
-    # format dateTime
+    data <- data[, c("Lat", "Long", "dateTime", "WaterTemp_C", "Discharge_m3s", 
+                     "DO_mgL", "DOsat_pct")]
+    # format dateTime, time zone is UTC
     data$dateTime <- ymd_hms(data$dateTime)
     #
     # convert units from cubic foot per second to m3 per second
@@ -68,18 +70,9 @@ dataLoad <- function(start.date = NULL, end.date = NULL){
     }
     data$Discharge_m3s <- ft3s_m3s(data$Discharge_m3s)
     #
-    # dateTime time zone is UTC
-    #
-    # pull in light data from NEON
-    PAR <- read.csv("./data_files/PAR_30min.csv", header = TRUE) # dateTime is UTC
-    # format dateTime
-    PAR$dateTime <- mdy_hm(PAR$dateTime)
-    #
-    data <- merge(data, PAR, by = "dateTime")
-    #
     # save .csv file locally
     write.csv(data, row.names = FALSE, 
-              file = "./data_files/MetabolismData_KansasRDesotoKS.csv")
+              file = paste0("./data_files/MetabolismData_", nickname, ".csv"))
     # return dataframe and exit function
     return(data)
   }
--- UnitTestingHW/metabolismModeling.R
+++ UnitTestingHW/metabolismModeling.R
@@ -1,7 +1,7 @@
 # script to run a bayseian model to estimate stream metabolism based on stream discharge, dissolved oxygen, dissolved oxygen saturation, and temperature data.
 #
 # Arguments
-# dataset       dataframe of stream metabolism time series parameters, 
+# data       dataframe of stream metabolism time series parameters, 
 #               assembled by dataLoad.R
 # na.fill       character vector of the algorithm that will be used by imputeTS::na.seasplit 
 #               to fill gaps in the time series data. see documentation of na.seasplit() 
@@ -16,44 +16,62 @@
 #library(StreamPULSE)
 #library(streamMetabolizer)
 
-metabolismModeling <- function(dataset, na.fill = "interpolation", lat, long){
+metabolismModeling <- function(data, na.fill = "interpolation", lat, long){
   # model will be broken by NA values, therefore
   # fill in NA gaps with imputeTS::na.seasplit according to algorithm of choice
-  dataset <- na.seasplit(dataset, algorithm = na.fill)
+  data <- imputeTS::na.seasplit(data, algorithm = na.fill)
   #
   # model will be broken by discharge values that are negative or 0, therefore
-  if(any(na.omit(dataset$Discharge_m3s) <= 0)){
+  if(any(na.omit(data$Discharge_m3s) <= 0)){
     # let user know that there are discharge values <= 0
     warning("Dataset has discharge values <= 0. Replacing these values with 0.00001.",
             call. = FALSE)
     # replace them with near-zero value
-    dataset$Discharge_m3s[dataset$Discharge_m3s <= 0] <- 0.00001
+    data$Discharge_m3s[data$Discharge_m3s <= 0] <- 0.00001
   }
   #
-  # convert UTC to solar time with streamMetabolizer::convert_UTC_to_solartime
-  dataset$solar.time <- convert_UTC_to_solartime(date.time = dataset$dateTime, 
-                                                 longitude = long,
-                                                 time.type = "mean solar")
+  # convert UTC to solar time
+  data$solar.time <- streamMetabolizer::convert_UTC_to_solartime(date.time = data$dateTime, 
+                                                                 longitude = data$Long,
+                                                                 time.type = "mean solar")
   # estimate PAR with streamMetabolizer::calc_solar_insolation
   # note: see some of the streamPULSE documentation for doing this
-  apparentsolartime <- convert_UTC_to_solartime(date.time = dataset$dateTime, 
-                                                longitude = long,
-                                                time.type = "apparent solar")
-  dataset$light <- calc_solar_insolation(app.solar.time = apparentsolartime,
-                                         latitude = lat,
-                                         format = "degrees")
+  apparentsolartime <- streamMetabolizer::convert_UTC_to_solartime(date.time = data$dateTime, 
+                                                                   longitude = data$Long,
+                                                                   time.type = "apparent solar")
+  data$light <- streamMetabolizer::calc_solar_insolation(app.solar.time = apparentsolartime,
+                                                         latitude = data$Lat,
+                                                         format = "degrees")
   # let user know that PAR was estimated based on latitude and time
   cat("PAR estimated based on latitude and time data.\n")
   #
-  # estimate depth from discharge data
+  # estimate depth (m) from discharge data
+  data$depth <- streamMetabolizer::calc_depth(data$Discharge_m3s)
   # 
   # convert DO saturation in percent to DO saturation in mg/L
+  data$DO.sat <- data$DO_mgL / (data$DOsat_pct*0.01)
+  #
+  # rename columns so they're recognized by streamMetabolizer
+  names(data)[names(data)=="DO_mgL"] <- "DO.obs"
+  names(data)[names(data)=="WaterTemp_C"] <- "temp.water"
   #
   # assemble streamMetabolizer call by setting model name and parameters
+  model_variables <- c("solar.time", "DO.obs", "DO.sat", "depth", "temp.water", "light")
+  #
+  # insert check if all model variables are actually in the dataframe or not, return error if F
+  #
+  # subset to just the data needed to fit the model
+  fitdata <- dplyr::select_(data, .dots = model_variables)
+  modelname <- streamMetabolizer::mm_name(type='mle', ode_method = "trapezoid")
+  modelspecs <- streamMetabolizer::specs(modelname)
+  # run the model
+  modelfit <- streamMetabolizer::metab(modelspecs, data = fitdata)
+  return(modelfit)
+  plot(modelfit@metab_daily$GPP)
   #
   # fit the model to the data (time consuming step, perhaps use MLE model for class project)
   #
-  # return dataframe of model results
+  # return model results
 }
 
 
